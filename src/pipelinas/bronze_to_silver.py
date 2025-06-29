9117a61d9c0f758e98bbf9c7899c0303c9300771
GabrielEliDias <gabrielelidias@proton.me>
2025-06-29 13:43:28 -0300

    fix: logic silver_to_gold.py and bronze_to_silver.py

--- src/pipelinas/bronze_to_silver.py
+++ src/pipelinas/bronze_to_silver.py
@@ -1,6 +1,5 @@
 # Bronze para silver, tratamento de dados brutos
 
-from sklearn.preprocessing import RobustScaler
 import pandas as pd
 from pathlib import Path
 
@@ -21,16 +20,6 @@ def bronze_to_silver(path_bronze: str, path_silver: str):
 
     # Ajustando as colunas em snake case
     df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
-
-    # Ajuste dos valores extremos para não serem fatores de distorcer da analise
-    scaler_amount = RobustScaler()
-    df["amount"] = scaler_amount.fit_transform(df[["amount"]])
-
-    # Normalização da coluna time
-    df["time"] = (df["time"] - df["time"].min()) / (df["time"].max() - df["time"].min())
-
-    # Randomização da tabela e seus valores
-    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
     # Salvando os dados na camada silver
     df.to_parquet(path_silver, index=False)
--- src/pipelinas/silver_to_gold.py
+++ src/pipelinas/silver_to_gold.py
@@ -1,11 +1,22 @@
 import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
 
 
 def prepare_gold_data(silver_path: str, gold_dir: str):
     # Carregar os dados da camada Silver
     df = pd.read_parquet(silver_path)
+
+    # Ajuste dos valores extremos para não serem fatores de distorcer da analise
+    scaler_amount = RobustScaler()
+    df["amount"] = scaler_amount.fit_transform(df[["amount"]])
+
+    # Normalização da coluna time
+    df["time"] = (df["time"] - df["time"].min()) / (df["time"].max() - df["time"].min())
+
+    # Randomização da tabela e seus valores
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 
     # Selecionar variáveis de entrada e alvo
     X = df.drop(columns=["class"])  # Features
