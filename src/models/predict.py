8aff60d22f9bc638568e3dbca0cbe3a7122fe457
GabrielEliDias <gabrielelidias@proton.me>
2025-07-18 23:46:42 -0300

    feat: creation of models manipulation for streamlit

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,7 +1,9 @@
 import joblib
 
 def load_model(path="models/fraud_model.pkl"):
+
     return joblib.load(path)
 
 def predict_instance(model, X):
+
     return model.predict(X), model.predict_proba(X)
