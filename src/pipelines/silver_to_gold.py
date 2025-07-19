ef7e28111aedb0c1815227a61b85ba8fabb9accd
JoseVColino <josevictorcolino@gmail.com>
2025-07-18 22:33:57 -0300

    refactor: saving the gold layer int the same way as the ml notebook

--- src/pipelines/silver_to_gold.py
+++ src/pipelines/silver_to_gold.py
@@ -3,36 +3,36 @@ import pandas as pd
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
+import joblib
 
 
 def prepare_gold_data(silver_path: str, gold_dir: str):
     # Carregar os dados da camada Silver
     df = pd.read_parquet(silver_path)
 
-    # Ajuste dos valores extremos para não serem fatores de distorcer da analise
-    time_scaler = MinMaxScaler()
-    scaler_amount = RobustScaler()
-
-    df['time_scaled'] = time_scaler.fit_transform(df[['time']])
-    df["amount"] = scaler_amount.fit_transform(df[["amount"]])
-
-    # Checagem do escalonamento
-    df[['amount_scaled', 'time_scaled']].describe()
-
-    # Normalização da coluna time
-    df["time"] = (df["time"] - df["time"].min()) / (df["time"].max() - df["time"].min())
-
-    # Randomização da tabela e seus valores
-    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-
     # Selecionar variáveis de entrada e alvo
     X = df.drop(columns=["class"])  # Features
     y = df["class"]                 # Target
-    (y.value_counts() / y.shape[0]) * 100
+    print('The percentage of non fraud and fraud is:', (y.value_counts() / y.shape[0]) * 100)
 
     # Dividir os dados em treino, validação e teste
-    X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=0.10, stratify=y, random_state=42)
-    X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.10, stratify=y_temp, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=11037)
+    
+    # Escalando features para treino efetivo dos modelos
+    time_scaler = MinMaxScaler()
+    amount_scaler = RobustScaler()
+
+    # fit para escalar os dados de treino
+    X_train['time_scaled'] = time_scaler.fit_transform(X_train[['time']])
+    X_train['amount_scaled'] = amount_scaler.fit_transform(X_train[['amount']])
+
+    # Transform para impedir data leak
+    X_test['time_scaled'] = time_scaler.transform(X_test[['time']])
+    X_test['amount_scaled'] = amount_scaler.transform(X_test[['amount']])
+
+    # Retirar colunas não escaladas
+    X_train = X_train.drop(columns=['time','amount'])
+    X_test = X_test.drop(columns=['time','amount'])
 
     # Criar diretório de saída
     gold_path = Path(gold_dir)
@@ -40,10 +40,11 @@ def prepare_gold_data(silver_path: str, gold_dir: str):
 
     # Salvar os conjuntos no formato Parquet
     X_train.to_parquet(gold_path / "X_train.parquet", index=False)
-    X_val.to_parquet(gold_path / "X_val.parquet", index=False)
     X_test.to_parquet(gold_path / "X_test.parquet", index=False)
     y_train.to_frame().to_parquet(gold_path / "y_train.parquet", index=False)
-    y_val.to_frame().to_parquet(gold_path / "y_val.parquet", index=False)
     y_test.to_frame().to_parquet(gold_path / "y_test.parquet", index=False)
 
+    # Salvando os scalers para poder escalar dados novos em produção
+    joblib.dump(time_scaler, gold_path / "time_scaler.pkl")
+    joblib.dump(amount_scaler, gold_path / "amount_scaler.pkl")
     print("Camada Gold criada com sucesso!")
