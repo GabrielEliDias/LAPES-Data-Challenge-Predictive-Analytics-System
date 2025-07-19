25edf288b02c15d5dac542c7c04ebbe00d56970d
GabrielEliDias <gabrielelidias@proton.me>
2025-07-18 23:54:21 -0300

    feat: creation of models manipulation for streamlit

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,7 @@
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
+
 def train_model(X_train, y_train, path="models/fraud_model.pkl"):
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
